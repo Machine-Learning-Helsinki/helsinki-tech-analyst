b9e4d361d43f7cf4e18f72be2edee78e13d32573
ayushghimiresharma95 <157111797+ayushghimiresharma95@users.noreply.github.com>
2025-09-12 12:26:56 +0300

    docker-compose is changed

--- front-end/App.tsx
+++ front-end/App.tsx
@@ -27,7 +27,7 @@ const App = () => {
     setAnswer(null);
 
     try {
-      const response = await fetch(`${API_URL}/ask`, {
+      const response = await fetch(`${API_URL}/api/ask`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
